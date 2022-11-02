8c2f9b774604752be08ab95639efab2cdb52a379
Victoriya Ryzhkova <victoriyabogonos@gmail.com>
2022-11-02 23:37:26 +0300

    feat: add component ErrorBoandary

--- packages/client/src/pages/Forum/Forum.tsx
+++ packages/client/src/pages/Forum/Forum.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react';
+import { ErrorBoundary } from 'src/components/ErrorBoundary';
 import { Button } from '../../components/Button';
 import { CreateTopic } from './components/CreateTopic';
 import { TopicList } from './components/TopicList';
@@ -12,17 +13,23 @@ export const Forum = () => {
   }, []);
 
   return (
-    <div className={styles.block}>
-      <div className={styles.block_button}>
-        <div className={styles.block_buttons_top}>
-          <Button>GO BACK</Button>
-          <Button>PLAY</Button>
+    <ErrorBoundary>
+      <div className={styles.block}>
+        <div className={styles.block_button}>
+          <div className={styles.block_buttons_top}>
+            <Button>GO BACK</Button>
+            <Button>PLAY</Button>
+          </div>
+          <Button onClick={handleCreateNewTopic}>POST NEW TOPIC</Button>
         </div>
-        <Button onClick={handleCreateNewTopic}>POST NEW TOPIC</Button>
+        <div className={styles.block_posts}>
+          {open ? (
+            <CreateTopic />
+          ) : (
+            <TopicList />
+          )}
+        </div>
       </div>
-      <div className={styles.block_posts}>
-        {open ? <CreateTopic /> : <TopicList />}
-      </div>
-    </div>
+    </ErrorBoundary>
   );
 };
--- packages/client/src/pages/Forum/components/TopicList/TopicList.tsx
+++ packages/client/src/pages/Forum/components/TopicList/TopicList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ErrorBoundary } from 'src/components/ErrorBoundary';
 import topics from 'src/static/topics.json';
 import { Topic } from '../Topic';
 import { ITopic } from '../Topic/types';
@@ -6,10 +7,12 @@ import styles from './topics.module.scss';
 
 export const TopicList = () => {
     return (
-        <div className={styles.list}>
-            {topics.map((topic: ITopic, index: number) => {
-              return <Topic key={index} topic={topic} />;
-            })}
-        </div>
+        <ErrorBoundary>
+            <div className={styles.list}>
+                {topics.map((topic: ITopic, index: number) => {
+                    return <Topic key={index} topic={topic} />;
+                })}
+            </div>
+        </ErrorBoundary>
     );
 };
