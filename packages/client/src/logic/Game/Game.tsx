0e1d5163ec48f182682222d6529305793bcbbf43
Vadim Maharram <asktask@icloud.com>
2022-11-06 13:25:26 +0500

    bug fix

--- packages/client/src/logic/Game/Game.tsx
+++ packages/client/src/logic/Game/Game.tsx
@@ -2,7 +2,6 @@ import { PlayerOne, PlayerTwo } from 'src/logic/Player/Player';
 import { GameImageProps } from './types';
 
 export class Game {
-<<<<<<< HEAD
 
   private ctx: CanvasRenderingContext2D;
   private width = 800;
@@ -20,75 +19,6 @@ export class Game {
 
   constructor(protected canvas: HTMLCanvasElement) {
     this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-=======
- 
-    private ctx: CanvasRenderingContext2D;
-    private width = 800;
-    private height = 500;
-    public isOver = false;
-    private background!: HTMLImageElement;
-
-    public fpsInterval!: number; 
-    public now!:number;
-    public then!:number;
-    public elapsed!:number;
-    
-
-    private playerOne: PlayerOne;
-    private playerTwo: PlayerTwo;
-    private gameOverBackgroundAudio: HTMLAudioElement | undefined;
-
-    constructor(protected canvas: HTMLCanvasElement) {
-        this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
-
-        this.playerOne = new PlayerOne();
-        this.playerOne.canvasHeight = this.height;
-        this.playerOne.canvasWidth = this.width;
-        this.playerOne.ctx = this.ctx;
-        
-        this.playerTwo = new PlayerTwo();
-        this.playerTwo.canvasHeight = this.height;
-        this.playerTwo.canvasWidth = this.width;
-        this.playerTwo.ctx = this.ctx;
-
-        this.background = new Image();
-        this.background.src = "/src/assets/images/game-background.png";
-
-
-
-    }
-    
-    destruct(){
-        window.removeEventListener('keydown',  this.playerOne.keyDownCustom);
-        window.removeEventListener('keydown',  this.playerTwo.keyDownCustom);
-        window.removeEventListener('keyup',  this.playerOne.keyUpCustom);
-        window.removeEventListener('keyup',  this.playerTwo.keyUpCustom);
-    }
-
-
-    start(gameRef:Game){
-        if (gameRef) {
-            gameRef.StartAnimating(10);  
-            const updater = () => {
-                gameRef.animate();
-                requestAnimationFrame( updater );  // for subsequent frames
-            };
-            requestAnimationFrame( updater ); // for the first frame https://stackoverflow.com/a/44975010
-          }
-    }
-
-    end(){     
-        this.isOver = true;
-        if(typeof this.gameOverBackgroundAudio === 'undefined'){
-            this.gameOverBackgroundAudio = new Audio("/src/assets/audio/game-over.mp3");
-            this.gameOverBackgroundAudio.play();
-        }
-     
-    }
->>>>>>> bug fix
 
     this.canvas.width = this.width;
     this.canvas.height = this.height;
@@ -157,7 +87,7 @@ export class Game {
     animate(){
         this.now = Date.now();
         this.elapsed = this.now - this.then;
-        if (this.elapsed > this.fpsInterval && !this.isOver){
+        if (this.elapsed > this.fpsInterval){
 
             this.then = this.now - (this.elapsed % this.fpsInterval); 
             this.ctx.clearRect (0,0,this.canvas.width, this.canvas.height); 
@@ -167,12 +97,15 @@ export class Game {
             this.playerTwo.animate();    
 
         }
+<<<<<<< HEAD
         // else if(this.isOver){
         //     // this.background = new Image();
         //     // this.background.src = "/src/assets/images/game-over-background.png";
         //     this.ctx.clearRect (0,0,this.canvas.width, this.canvas.height); 
         //     this.ctx.drawImage(this.background, 0, 0, this.canvas.width, this.canvas.height);
         // }
+>>>>>>> bug fix
+=======
 >>>>>>> bug fix
     }
   }
