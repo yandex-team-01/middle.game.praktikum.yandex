f568de238b405be049f449b38d8fbd493eccc33f
Olga <helgamir@Olgas-MacBook-Air.local>
2022-11-16 15:06:17 +0100

    fix service worker

--- packages/client/src/utils/serviceWorker.ts
+++ packages/client/src/utils/serviceWorker.ts
@@ -2,7 +2,7 @@ export const startServiceWorker = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/sw.js')
+        .register('/sw.ts')
         .then(registration => {
           console.log(
             'ServiceWorker registration successful with scope: ',
