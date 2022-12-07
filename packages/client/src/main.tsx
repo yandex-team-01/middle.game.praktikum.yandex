f8b2e1fea0a4652fe28e0c33b311b1f836a49c99
Diana Garbuz <GDI.1999@yandex.ru>
2022-12-07 21:17:13 +0300

    ssr routing 1

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import { i18next } from '../i18next.config';
 
 import { App } from 'src/App';
 import 'normalize.css';
 import './index.module.scss';
-import { setupStore } from 'src/store/store';
+import { setupStore, history } from 'src/store/store';
 import { ErrorBoundary } from 'src/components/ErrorBoundary';
 import { startServiceWorker } from 'src/utils/serviceWorker';
 import { PreloadedState } from 'src/store/types';
+
+import { ConnectedRouter } from 'connected-react-router';
 
 const defineStore = window.__PRELOADED_STATE__ as PreloadedState;
 console.log('window defaultStore', window.__PRELOADED_STATE__);
@@ -19,19 +21,23 @@ delete window.__PRELOADED_STATE__;
 console.log('window delete defaultStore', window.__PRELOADED_STATE__);
 const store = setupStore(defineStore);
 
+console.log('history', history);
+
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <React.Suspense>
-      <BrowserRouter>
-        <Provider store={store}>
+      {/* <BrowserRouter> */}
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
           <I18nextProvider i18n={i18next}>
             <ErrorBoundary>
               <App />
             </ErrorBoundary>
           </I18nextProvider>
-        </Provider>
-      </BrowserRouter>
+        </ConnectedRouter>
+      </Provider>
+      {/* </BrowserRouter> */}
     </React.Suspense>
   </React.StrictMode>
 );
--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -1,4 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createMemoryHistory } from 'history';
 
 import { usersReducer } from './users/UsersSlice';
 import { authReducer } from './auth/AuthSlice';
@@ -6,17 +8,28 @@ import { errorReducer } from './error/ErrorSlice';
 import { forumReducer } from './forum/ForumSlice';
 import { PreloadedState } from './types';
 
-const rootReducer = combineReducers({
-  auth: authReducer,
-  error: errorReducer,
-  forum: forumReducer,
-  users: usersReducer,
-});
+// @ts-ignore
+const rootReducer = history => {
+  return combineReducers({
+    router: connectRouter(history),
+    auth: authReducer,
+    error: errorReducer,
+    forum: forumReducer,
+    users: usersReducer,
+  });
+};
 
+export const history = createMemoryHistory({
+  initialEntries: ['/'],
+}); //createBrowserHistory();
+
+// @ts-ignore
 export const setupStore = (preloadedState: PreloadedState) => {
   return configureStore({
-    reducer: rootReducer,
+    reducer: rootReducer(history),
     preloadedState: preloadedState || {},
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().prepend(routerMiddleware(history)),
   });
 };
 
