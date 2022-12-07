ff8e34005fed52b3aa88ec9e29edde7c0e0c3ab6
VADIM MAHARRAM <asktask@icloud.com>
2022-12-07 13:37:56 +0500

    bug fix collision result

--- packages/client/src/logic/ClashesController.ts
+++ packages/client/src/logic/ClashesController.ts
@@ -109,7 +109,7 @@ export class ClashesController {
       ['enemy_huggy', 'enemy_kissy'].includes(npc.type) &&
       prevNpc.type === 'friend'
     ) {
-      prevNpc.speed = prevNpc.speed + 1;
+      npc.speed = prevNpc.speed + 1;
       this.setColisionImgCoordinats(prevNpc, true);
       this.deleteAndRestoreNpc(prevNpc);
     }
