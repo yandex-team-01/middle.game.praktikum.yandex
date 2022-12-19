21b1605fce28b0905db215cc6880b0a719c2d27a
weqwew <weqwew@tofeat.com>
2022-12-19 06:25:44 -0500

    fix: return back express router

--- packages/server/middlewares/index.ts
+++ packages/server/middlewares/index.ts
@@ -3,3 +3,17 @@ import cookieParserMiddleware from 'cookie-parser';
  
 export const cookieParser: RequestHandler = cookieParserMiddleware(); 
 export { auth } from './auth';
+
+import { Router, ErrorRequestHandler, RequestHandler } from 'express';
+import { renderHtml } from '../utils/renderHtml';
+import { allRoutes } from '../constants/allRoutes';
+
+export const router: Router = Router();
+
+const middlewares: Array<RequestHandler | ErrorRequestHandler> = [];
+
+export function appRoutes(router: Router) {
+  router.get(allRoutes, middlewares, renderHtml);
+}
+
+appRoutes(router);
