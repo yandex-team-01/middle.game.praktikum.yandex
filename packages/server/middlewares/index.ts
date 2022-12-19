70093538b40417310fe3f359d3900db0c81957ca
weqwew <weqwew@tofeat.com>
2022-12-19 06:25:44 -0500

    fix: return back express router

--- packages/server/middlewares/index.ts
+++ packages/server/middlewares/index.ts
@@ -1,10 +1,5 @@
-import type { RequestHandler } from 'express';
-import cookieParserMiddleware from 'cookie-parser';
-import i18nextMiddleware from 'i18next-http-middleware';
-import Backend from 'i18next-fs-backend';
-
-export const cookieParser: RequestHandler = cookieParserMiddleware();
-
+import type { RequestHandler } from 'express'; 
+import cookieParserMiddleware from 'cookie-parser'; 
+ 
+export const cookieParser: RequestHandler = cookieParserMiddleware(); 
 export { auth } from './auth';
-export const i18nMiddleware = i18nextMiddleware;
-export const backend = Backend;
