07f8aaca8512d378183b4b5b16ac292af7ec0867
samgxlden <samgxlden@gmail.com>
2022-12-22 15:27:26 +0000

    fix: change dir global types i18n

--- packages/client/src/global.d.ts
+++ packages/client/src/global.d.ts
@@ -1,7 +1,10 @@
 import { PreloadedState } from 'src/store/types';
+import { Resource } from 'i18next';
 
 declare global {
   interface Window {
     __PRELOADED_STATE__?: PreloadedState;
+    initialLanguage?: string;
+    initialI18nStore?: Resource;
   }
 }
