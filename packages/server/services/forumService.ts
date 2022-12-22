f05cd820f82b3229b13c4c37fa33e6a8d0fb37ec
Victoriya Ryzhkova <victoriyabogonos@gmail.com>
2022-12-22 18:33:18 +0300

    рефакторинг после ревью по задаче 113

--- packages/server/services/forumService.ts
+++ packages/server/services/forumService.ts
@@ -1,85 +1,61 @@
-import type { Response } from "express";
+import type { Request, Response } from "express";
 import { commentRepos } from "../models/modelComment";
 import { topicRepos } from "../models/modelTopic";
 
 export class forumService {
-    //@ts-ignore
-    getAllTopics = (_, res: Response) => {
+    getAllTopics = (_: Request, res: Response) => {
         topicRepos
             .getAll()
             .then(topics => res.status(200).json(topics))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 
-    //@ts-ignore
-    getTopic = (_req, res: Response) => {
+    getTopic = (_req: Request, res: Response) => {
         topicRepos
-            .get(_req.id)
+            .get(_req.params.id)
             .then(topic => res.status(200).json(topic))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 
-    //@ts-ignore
-    createTopic = (_req, res: Response) => {
+    createTopic = (_req: Request, res: Response) => {
         topicRepos
-            .create(_req.value)
+            .create(_req.body)
             .then(topic => res.status(200).json(topic))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 
-    //@ts-ignore
-    deleteTopic = (_req, res: Response) => {
+    deleteTopic = (_req: Request, res: Response) => {
         topicRepos
-            .delete(_req.id)
+            .delete(_req.params.id)
             .then(() => res.status(200))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 
-    //@ts-ignore
-    updateTopic = (_req, res: Response) => {
-        topicRepos
-            .update(_req.id, _req.value)
-            .then(topic => res.status(200).json(topic))
-            .catch(err => res.status(500).json({ error: ["db error", err] }));
-    };
-
-    //@ts-ignore
-    getAllComment = (_req, res: Response) => {
+    getAllComment = (_req: Request, res: Response) => {
         commentRepos
             .getAll()
             .then(comments => res.status(200).json(comments))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 
-    //@ts-ignore
-    getComment = (_req, res: Response) => {
+    getComment = (_req: Request, res: Response) => {
         commentRepos
-            .get(_req.id)
+            .get(_req.params.id)
             .then(comment => res.status(200).json(comment))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 
-    //@ts-ignore
-    createComment = (_req, res: Response) => {
+    createComment = (_req: Request, res: Response) => {
         commentRepos
-            .create(_req.value)
+            .create(_req.body)
             .then(comment => res.status(200).json(comment))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 
-    //@ts-ignore
-    deleteComment = (_req, res: Response) => {
+    deleteComment = (_req: Request, res: Response) => {
         commentRepos
-            .delete(_req.id)
+            .delete(_req.params.id)
             .then(() => res.status(200))
-            .catch(err => res.status(500).json({ error: ["db error", err] }));
-    };
-
-    //@ts-ignore
-    updateComment = (_req, res: Response) => {
-        commentRepos
-            .update(_req.id, _req.value)
-            .then(comment => res.status(200).json(comment))
             .catch(err => res.status(500).json({ error: ["db error", err] }));
     };
 };
