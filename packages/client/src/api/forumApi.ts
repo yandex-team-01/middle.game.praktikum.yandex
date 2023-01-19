48c1dd49edecc4a9070ebddbbf33f7378ce67a59
Helga108 <helgamir@gmail.com>
2023-01-19 16:29:15 +0100

    update urls

--- packages/client/src/api/forumApi.ts
+++ packages/client/src/api/forumApi.ts
@@ -4,69 +4,53 @@ import { ITopic } from 'src/pages/Forum/part/Topic/types';
 import { fetchApi } from 'src/store/utils';
 
 export const getTopics = () => {
-  return fetchApi<ITopic[]>(
-    '/api/forum/topic',
-    {
-      credentials: 'include',
-      method: 'GET',
-      headers: {
-        ...defaultHeaders,
-        'Content-Type': 'text/plain;charset=UTF-8',
-        Accept: 'application/json',
-      },
+  return fetchApi<ITopic[]>('/api/forum/topic', {
+    credentials: 'include',
+    method: 'GET',
+    headers: {
+      ...defaultHeaders,
+      'Content-Type': 'text/plain;charset=UTF-8',
+      Accept: 'application/json',
     },
-    `${import.meta.env.VITE_SERVER}`
-  );
+  });
 };
 
 export const createTopics = (topic: ITopic) => {
-  return fetchApi<ITopic>(
-    '/api/forum/topic',
-    {
-      method: 'POST',
-      headers: {
-        mode: 'cors',
-        'Content-Type': 'application/json;charset=utf-8',
-        Accept: 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        referrerPolicy: 'strict-origin-when-cross-origin',
-      },
-      body: JSON.stringify(topic),
+  return fetchApi<ITopic>('/api/forum/topic', {
+    method: 'POST',
+    headers: {
+      mode: 'cors',
+      'Content-Type': 'application/json;charset=utf-8',
+      Accept: 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      referrerPolicy: 'strict-origin-when-cross-origin',
     },
-    `${import.meta.env.VITE_SERVER}`
-  );
+    body: JSON.stringify(topic),
+  });
 };
 
 export const getComments = (id_topic: string) => {
-  return fetchApi<IComment[]>(
-    `/api/forum/comment?id_topic=${id_topic}`,
-    {
-      credentials: 'include',
-      method: 'GET',
-      headers: {
-        ...defaultHeaders,
-        'Content-Type': 'text/plain;charset=UTF-8',
-        Accept: 'application/json',
-      },
+  return fetchApi<IComment[]>(`/api/forum/comment?id_topic=${id_topic}`, {
+    credentials: 'include',
+    method: 'GET',
+    headers: {
+      ...defaultHeaders,
+      'Content-Type': 'text/plain;charset=UTF-8',
+      Accept: 'application/json',
     },
-    `${import.meta.env.VITE_SERVER}`
-  );
+  });
 };
 
 export const createComment = (comment: IComment) => {
-  return fetchApi<IComment>(
-    '/api/forum/comment',
-    {
-      method: 'POST',
-      headers: {
-        mode: 'cors',
-        'Content-Type': 'application/json;charset=utf-8',
-        Accept: 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        referrerPolicy: 'strict-origin-when-cross-origin',
-      },
-      body: JSON.stringify(comment),
+  return fetchApi<IComment>('/api/forum/comment', {
+    method: 'POST',
+    headers: {
+      mode: 'cors',
+      'Content-Type': 'application/json;charset=utf-8',
+      Accept: 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      referrerPolicy: 'strict-origin-when-cross-origin',
     },
-    `${import.meta.env.VITE_SERVER}`
-  );
+    body: JSON.stringify(comment),
+  });
 };
--- packages/client/src/api/themeApi.ts
+++ packages/client/src/api/themeApi.ts
@@ -3,14 +3,10 @@ import { defaultHeaders } from 'src/constants/http';
 import { fetchApi } from 'src/store/utils';
 
 export const updateTheme = (data: { id: number; theme: string }) => {
-  return fetchApi(
-    '/user/theme',
-    {
-      method: 'POST',
-      headers: defaultHeaders,
-      credentials: 'include',
-      body: JSON.stringify(data),
-    },
-    'https://localhost:3001/api'
-  );
+  return fetchApi('/user/theme', {
+    method: 'POST',
+    headers: defaultHeaders,
+    credentials: 'include',
+    body: JSON.stringify(data),
+  });
 };
